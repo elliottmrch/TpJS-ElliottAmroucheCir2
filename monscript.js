10385811da12d792e99813a2cf720d50a093876e
Elliott Amrouche <elliottamrouche27@gmail.com>
2026-02-04 12:19:21 +0100

    TP10

--- monscript.js
+++ monscript.js
@@ -18,14 +18,48 @@ if (isRainingToday) {
     console.log("Where is my umbrella?");
 }
 
+
 let age = prompt("Quel âge as-tu ?");
 
-if (age > 0 && age < 18) {
-    alert(`L’utilisateur a ${age} an(s), il est mineur!`);
-} else if (age >= 18 && age < 62) {
-    alert(`L’utilisateur a ${age} an(s), il est majeur!`);
-} else if (age >= 62) {
-    alert(`L’utilisateur a ${age} an(s), il est majeur mais aussi retraité !!`);
-} else {
-    alert("L'âge saisi n'est pas valide.");
+// if (age > 0 && age < 18) {
+//     alert(`L’utilisateur a ${age} an(s), il est mineur!`);
+// } else if (age >= 18 && age < 62) {
+//     alert(`L’utilisateur a ${age} an(s), il est majeur!`);
+// } else if (age >= 62) {
+//     alert(`L’utilisateur a ${age} an(s), il est majeur mais aussi retraité !!`);
+// } else {
+//     alert("L'âge saisi n'est pas valide.");
+// }
+
+
+// if (age == 18) {
+//     alert("il vient d’être majeur");
+// } else if (age == 25) {
+//     alert("il a un quart de siècle");
+// } else if (age == 50) {
+//     alert("il a un demi siècle");
+// } else if (age == 62) {
+//     alert("il vient d’être à la retraite");
+// } else if (age == 100) {
+//     alert("il vient d’être centenaire");
+// }
+
+switch (age) {
+    case '18':
+        alert("il vient d’être majeur");
+        break;
+    case '25':
+        alert("il a un quart de siècle");
+        break;
+    case '50':
+        alert("il a un demi siècle");
+        break;
+    case '62':
+        alert("il vient d’être à la retraite");
+        break;
+    case '100':
+        alert("il vient d’être centenaire");
+        break;
+    default:
+        alert("Âge non spécial");
 }
