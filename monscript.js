540b1b0fc9c36a4bbd028f55a19405d02689acf7
Elliott Amrouche <elliottamrouche27@gmail.com>
2026-02-04 12:00:22 +0100

    TP8

---
+++ monscript.js
@@ -0,0 +1,19 @@
+let number = 5;
+let text = '5';
+let isRainingToday = true;
+
+if (number == text) {
+    console.log("égal avec ==");
+} else {
+    console.log("pas égal avec ==");
+}
+
+if (number === text) {
+    console.log("égal avec ===");
+} else {
+    console.log("pas égal avec ===");
+}
+
+if (isRainingToday) {
+    console.log("Where is my umbrella?");
+}
