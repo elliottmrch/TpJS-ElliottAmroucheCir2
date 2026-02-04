d392ddbda77ef7b15c0d47b0fad19c0a36a7b1ea
Elliott Amrouche <elliottamrouche27@gmail.com>
2026-02-04 12:07:46 +0100

    TP9

--- monscript.js
+++ monscript.js
@@ -17,3 +17,15 @@ if (number === text) {
 if (isRainingToday) {
     console.log("Where is my umbrella?");
 }
+
+let age = prompt("Quel âge as-tu ?");
+
+if (age > 0 && age < 18) {
+    alert(`L’utilisateur a ${age} an(s), il est mineur!`);
+} else if (age >= 18 && age < 62) {
+    alert(`L’utilisateur a ${age} an(s), il est majeur!`);
+} else if (age >= 62) {
+    alert(`L’utilisateur a ${age} an(s), il est majeur mais aussi retraité !!`);
+} else {
+    alert("L'âge saisi n'est pas valide.");
+}
