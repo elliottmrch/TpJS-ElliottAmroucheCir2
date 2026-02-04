6b6cdc839b7d626d9db8620ad5088c48bf42981a
Elliott Amrouche <elliottamrouche27@gmail.com>
2026-02-04 11:36:37 +0100

    TP6

--- monscript.js
+++ monscript.js
@@ -20,3 +20,25 @@ maDateV2.jour = "Mercredi";
 maDateV2.mois = "Février";
 
 console.log(maDateV1.jour, maDateV1.mois, maDateV2.jour, maDateV2.mois);
+
+let nbr1;
+let nbr2;
+let nbr3;
+let nbr4;
+
+nbr1 = 3;
+nbr2 = 7;
+nbr3 = 8;
+nbr4 = 21;
+
+console.log(nbr1 + nbr2);
+console.log(nbr4 - nbr3);
+
+nbr2 = nbr1 * nbr2;
+console.log(nbr2);
+
+nbr2 += 1;
+console.log(nbr2);
+
+console.log(nbr4 / nbr1);
+console.log(nbr4 % nbr1);
