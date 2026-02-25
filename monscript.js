1418a9f275793e95952d13f51d889e83ef1701c5
Elliott Amrouche <elliottamrouche27@gmail.com>
2026-02-25 15:59:30 +0100

    TP Bonus : Eviter doublon d'item + Gérer la case

--- monscript.js
+++ monscript.js
@@ -32,6 +32,10 @@ function addProduct() {
     let produit = myInput.value;
     if (produit == "") {
         alert("Erreur de saisie");
+    }
+    //pas de doublon (majuscule et minuscule sont la même chose)
+    else if (liste.innerHTML.toLowerCase().includes(produit.toLowerCase())) {
+        alert("Produit déjà dans la liste");
     } else {
         alert("Produit ajouté")
         let nvLi = document.createElement("li");
