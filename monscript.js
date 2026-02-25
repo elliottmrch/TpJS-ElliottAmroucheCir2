d6e954eceab93c7b2e8c98430e31c91db23be04f
Elliott Amrouche <elliottamrouche27@gmail.com>
2026-02-25 11:51:20 +0100

    TP15

--- monscript.js
+++ monscript.js
@@ -1,6 +1,12 @@
 /* Toutes les variables */
 let li = document.querySelector("#listeCourses li");
 
+let dateJour = new Date(Date.now());
+let dateJourFr = dateJour.toLocaleDateString('fr-FR')
+
+let h2 = document.querySelector("h2");
+let titreH2 = h2.innerHTML;
+h2.innerHTML = `${titreH2} : ${dateJourFr}`;
 
 /* Tous les évènements */
 li.addEventListener('click', () => {
