90a965876b3bccc93ea29ebbec53f389e4e881f3
Elliott Amrouche <elliottamrouche27@gmail.com>
2026-02-25 12:25:37 +0100

    TP16

--- monscript.js
+++ monscript.js
@@ -8,10 +8,38 @@ let h2 = document.querySelector("h2");
 let titreH2 = h2.innerHTML;
 h2.innerHTML = `${titreH2} : ${dateJourFr}`;
 
+let myInput = document.querySelector("#myInput");
+let btn = document.querySelector(".btn");
+let liste = document.querySelector("#listeCourses");
+
 /* Tous les évènements */
 li.addEventListener('click', () => {
     li.classList.toggle("itemCheck");
 })
 
+btn.addEventListener('click', () => {
+    addProduct();
+})
+
+myInput.addEventListener('keyup', (event) => {
+    if (event.key == "Enter") {
+        addProduct();
+    }
+})
+
 /* Les fonctions */
-// A voir plus tard
+function addProduct() {
+    let produit = myInput.value;
+    if (produit == "") {
+        alert("Erreur de saisie");
+    } else {
+        // alert("Produit ajouté")
+        let nvLi = document.createElement("li");
+        nvLi.innerHTML = produit;
+        nvLi.addEventListener('click', () => {
+            nvLi.classList.toggle("itemCheck");
+        });
+        liste.appendChild(nvLi);
+        myInput.value = "";
+    }
+}
